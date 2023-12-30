6a9a7b01edb67d791ca9f8d2962d1e4b4b6454db
Ioannis Eleftheriou <me@yath.xyz>
2023-12-30 06:17:04 +0000

    Reformat flake text

--- flake.nix
+++ flake.nix
@@ -17,37 +17,37 @@
       # TODO make this ISA agnostic - it should work for now
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system}.extend (final: prev:
-        let
-          dataDir = "var/lib/xppend1v2";
-        in
-        {
-        xp-pen-deco-01-v2-driver = prev.xp-pen-deco-01-v2-driver.overrideAttrs (oldAttrs: rec {
-          version = "3.4.9-231023";
+        let dataDir = "var/lib/xppend1v2";
+        in {
+          xp-pen-deco-01-v2-driver = prev.xp-pen-deco-01-v2-driver.overrideAttrs
+            (oldAttrs: rec {
+              version = "3.4.9-231023";
 
-          src = prev.fetchzip {
-            url = "https://www.xp-pen.com/download/file/id/1936/pid/440/ext/gz.html#.tar.gz";
-            name = "xp-pen-deco-01-v2-driver-${version}.tar.gz";
-            sha256 = "sha256-A/dv6DpelH0NHjlGj32tKv37S+9q3F8cYByiYlMuqLg=";
-          };
-          installPhase = ''
-            runHook preInstall
+              src = prev.fetchzip {
+                url =
+                  "https://www.xp-pen.com/download/file/id/1936/pid/440/ext/gz.html#.tar.gz";
+                name = "xp-pen-deco-01-v2-driver-${version}.tar.gz";
+                sha256 = "sha256-A/dv6DpelH0NHjlGj32tKv37S+9q3F8cYByiYlMuqLg=";
+              };
+              installPhase = ''
+                runHook preInstall
 
-            mkdir -p $out/{opt,bin}
-            cp -r App/usr/lib/pentablet/{PenTablet,resource.rcc,conf} $out/opt
-            chmod +x $out/opt/PenTablet
-            cp -r App/lib $out/lib
-            sed -i 's#usr/lib/pentablet#${dataDir}#g' $out/opt/PenTablet
+                mkdir -p $out/{opt,bin}
+                cp -r App/usr/lib/pentablet/{PenTablet,resource.rcc,conf} $out/opt
+                chmod +x $out/opt/PenTablet
+                cp -r App/lib $out/lib
+                sed -i 's#usr/lib/pentablet#${dataDir}#g' $out/opt/PenTablet
 
-            runHook postInstall
+                runHook postInstall
 
-          '';
-          postFixup = ''
-            makeWrapper $out/opt/PenTablet $out/bin/xp-pen-deco-01-v2-driver \
-              "''${qtWrapperArgs[@]}" \
-              --run 'if [ "$EUID" -ne 0 ]; then echo "Please run as root."; exit 1; fi' \
-              --run 'if [ ! -d /${dataDir} ]; then mkdir -p /${dataDir}; cp -r '$out'/opt/conf /${dataDir}; chmod u+w -R /${dataDir}; fi'
-          '';
-          });
+              '';
+              postFixup = ''
+                makeWrapper $out/opt/PenTablet $out/bin/xp-pen-deco-01-v2-driver \
+                  "''${qtWrapperArgs[@]}" \
+                  --run 'if [ "$EUID" -ne 0 ]; then echo "Please run as root."; exit 1; fi' \
+                  --run 'if [ ! -d /${dataDir} ]; then mkdir -p /${dataDir}; cp -r '$out'/opt/conf /${dataDir}; chmod u+w -R /${dataDir}; fi'
+              '';
+            });
         });
       finalname = "yanni";
     in {
