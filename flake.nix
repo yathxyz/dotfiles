295ac713c99768ec869acb167134c49a0f10fbc4
Ioannis Eleftheriou <me@yath.xyz>
2024-01-22 06:42:39 +0000

    Simplify path name and follow input

--- flake.nix
+++ flake.nix
@@ -14,10 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-colors = {
-      url = "github:misterio77/nix-colors";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-colors.url = "github:misterio77/nix-colors";
 
   };
 
