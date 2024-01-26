789a1a789ddcb0e4bbea2cb77ad6757b8696f6d2
Ioannis Eleftheriou <me@yath.xyz>
2024-01-26 04:30:29 +0000

    Add secrets

--- home.nix
+++ home.nix
@@ -19,6 +19,10 @@
       format = "binary";
       path = "/home/${defaultName}/.config/rclone/rclone.conf";
     };
+
+    secrets."OPENAI_API_KEY".path = "%r/secrets/OPENAI_API_KEY";
+    secrets."GITHUB_TOKEN".path = "%r/secrets/GITHUB_TOKEN";
+    secrets."BRAVE_SYNC_KEY".path = "%r/secrets/BRAVE_SYNC_KEY";
   };
 
   home.packages = with pkgs; [
