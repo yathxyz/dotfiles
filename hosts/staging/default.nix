8c5bbd28817a1a693e1de16895e491d6c9d14952
Ioannis Eleftheriou <me@yath.xyz>
2024-03-12 10:27:52 +0000

    Make emacs work

--- hosts/staging/default.nix
+++ hosts/staging/default.nix
@@ -1,14 +1,10 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
+#      inputs.home-manager.nixosModules.default
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -23,21 +19,22 @@
 
   boot.loader.efi.canTouchEfiVariables = true;
 
-  home-manager = {
-    extraSpecialArgs = { inherit inputs; };
-    users = { 
-      "yanni" = {
-        home.sessionVariables = {
-	  WORKDIR = "$HOME/work/";
-	  ZOTERODIR = "$HOME/Zotero/";
-	  STUFFDIR = "$HOME/stuff/";
-	  EDITOR = "nvim";
-        };
-
-	home.stateVersion = "23.05";
-      };
-    };
-  };
+#  home-manager = {
+#    extraSpecialArgs = { inherit inputs; };
+#    users = { 
+#      "yanni" = {
+#        home.sessionVariables = {
+#	  WORKDIR = "$HOME/work/";
+#	  ZOTERODIR = "$HOME/Zotero/";
+#	  STUFFDIR = "$HOME/stuff/";
+#	  EDITOR = "nvim";
+#
+#        };
+#
+#	home.stateVersion = "23.05";
+#      };
+#    };
+#  };
 
   networking.hostName = "battlestation"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -124,6 +121,7 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       firefox
+      signal-desktop
     #  thunderbird
     ];
   };
@@ -139,15 +137,20 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     neovim
-    git
     emacs
+    git
   ];
+
+  environment.variables = {
+    EDITOR = "nvim";
+    VISUAL = "emacs";
+
+    WORKDIR = "$HOME/work/";
+    ZOTERODIR = "$HOME/Zotero/";
+    STUFFDIR = "$HOME/stuff/";
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -173,12 +176,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
