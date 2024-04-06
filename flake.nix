034ecf9d3fccd29ed9e102fdcc8049a793954f68
Ioannis Eleftheriou <me@yath.xyz>
2024-04-06 03:15:00 +0100

    Revert to nixos-unstable as input

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
