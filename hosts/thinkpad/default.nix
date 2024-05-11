169349c2b1852fab7721c758c5717fe36c45af65
Ioannis Eleftheriou <me@yath.xyz>
2024-05-11 20:30:01 +0100

    Restore minimal setup for thinkpad host

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -48,10 +48,26 @@
 
   environment.pathsToLink = [ "/libexec" ];
 
+  services.displayManager = {
+    defaultSession = "none+i3";
+  };
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+	i3status
+	i3lock
+      ];
+    };
+
   };
 
   services.dbus.enable = true;
@@ -120,6 +136,7 @@
     gnupg-pkcs11-scd
     htop
     keepassxc
+    kitty
     libtool
     neovim
     nh
