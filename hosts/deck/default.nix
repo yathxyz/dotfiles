78b43607eb464ea1cd9f50dc242d96aa70f80297
Ioannis Eleftheriou <me@yath.xyz>
2024-08-04 20:41:13 +0100

    Add ntfs support to deck

--- hosts/deck/default.nix
+++ hosts/deck/default.nix
@@ -14,6 +14,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
 
   networking.hostName = "deck"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
