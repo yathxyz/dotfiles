77c8ae2b6f79900ef40dd5f69703b7ca5c58eed7
Ioannis Eleftheriou <me@yath.xyz>
2024-08-04 18:32:45 +0100

    add deck config

--- flake.nix
+++ flake.nix
@@ -84,6 +84,13 @@
             }
 	  ];
 	};
+
+        deck = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/deck
+          ];
+        };
       };
 
       homeConfigurations = {
